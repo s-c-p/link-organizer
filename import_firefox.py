e095c8b6cd83b5a08c2b66b3660120ff11f70651
s-c-p <ps45@binkmail.com>
2017-09-14 16:09:28 +0000

    refactor

--- import_firefox.py
+++ import_firefox.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
 # TODO: try lxml
+from bs4 import BeautifulSoup
 
 context = list()
-
-def what_does_line_tell(line):
-	soup = BeautifulSoup(line)
-	if soup.find_all('a'):
-		return "link container"
-	elif soup.find_all('h3'):
-		return "title/category/folder container"
-	elif soup.find_all('dl') or "</dl" in line.lower():
-		return "nesting hint"
-	else:
-		return False
-
-interesting = what_does_line_tell
+debutant = str()	# empty string, representing the very first debutant i.e.
+					# the namesless folder/category where all bookmarks go by
+					# default
 
 def process(line):
-	if not interesting(line):
-		return False
-	type_of_line = what_does_line_tell(line)
-	if type_of_line == "link container":
-		obj = BeautifulSoup(line).find_all("a")[0]
-		url = obj.get('href')
-		title = obj.get_text()
-		timestamp = obj.get('add_date')
-		return (url, title, timestamp)
-	elif type_of_line == "title/category/folder container":
-		debut_ready = BeautifulSoup(line).find_all("h3")[0].get_text()
+	soup = BeautifulSoup(line)
+	if soup.find_all("a"):
+		# this line is a link container
+		_obj = soup.find_all("a")[0]
+		url = _obj.get('href')
+		title = _obj.get_text()
+		timestamp = _obj.get('add_date')
+		return (url, title, timestamp, context)
+	elif soup.find_all('h3'):
+		# this line contains category/folder related info
+		debut_ready = soup.find_all("h3")[0].get_text()
 		global debutant
 		debutant = debut_ready
+		# we are not appending ``debut_ready`` to ``context`` just yet because
+		# <DL> appears first, at 0 indent, before any <H3> and hence, we wait
+		# until a DL is found JUST after H3
 		return None
-	elif type_of_line == "nesting hint":
+	elif soup.find_all('dl'):
+		# nesting begin hint
 		global context
-		if "<dl>" in line.lower():
-			context.append(debut_ready)
-			# pass	# because before this line, the previousLine's processing would have invoked creation of debutant
-		else:
-			context.pop()
+		context.append(debutant)
 		return None
+	elif "</dl" in line.lower():
+		# de-nesting begin hint
+		context.pop()
+		return None
+	else:
+		# this line is not intersting
+		return False
 	return
 
 def main(file_path):
 	with open(file_path, mode='rt') as fh:
-		previousLine = str()
 		for aLine in fh:
-			process(aLine)
+			ans = process(aLine)
+			if ans:
+				# add to ``unorganized`` db
+				print(ans[1])
 	assert len(context) == 0	# since last DL has been closed
 	return
+
+if __name__ == '__main__':
+	main('./samples/firefox.html')
