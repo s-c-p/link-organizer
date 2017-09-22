f1053054d74ee8fe337c5735f9ff2cdbea5cdd1a
s-c-p <ps45@binkmail.com>
2017-09-22 23:11:05 +0530

    yo

--- model/staging.py
+++ model/staging.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
 """
-when bulk_import.py runs, it returns a list of tuples, the contents of
-which are as follows
-	url
-	title
-	timestamp
+when bulk_import.py runs, it returns a list of Raw/Crude objects, the contents
+of which are
+	url (may be partial)
+	title (as stored by user and not of the webpage itself)
+	timestamp (as reported by browser)
 	context (which helps in suggesting tags)
-and an additional field which would indicate the date/session when a given set
-of bookmarks were imported to the staging area (this is important from UX POV
-and will not be forwarded to organized.db)
-	SESSION_INDICATOR
-note that SESSION_INDICATOR will be same for all links imported at once so its
-a one-to-many relationship
 
 the purpose of this file is to
 	store above schema in a database
@@ -59,13 +53,18 @@ def initNewImport(file_path):
 	with sqliteDB(dbFile) as cur:
 		try:
 			cur.execute(
-			"INSERT INTO imports (file_contents, ts_on_zAxis, hash) VALUES (?,?,?)",
-			[plain_text, timestamp, hashVal])
+				"INSERT INTO imports " 					\
+				"(file_contents, ts_on_zAxis, hash) "	\
+				"VALUES (?,?,?)", [plain_text, timestamp, hashVal]
+			)
 		except sqlite3.IntegrityError:
 			# UNIQUE constraint failed: imports.hash
-			x = cur.execute("SELECT ts_on_zAxis FROM imports WHERE hash=?", [hashVal])
+			x = cur.execute(
+				"SELECT ts_on_zAxis FROM imports WHERE hash=?",
+				[hashVal]
+			)
 			x = x.fetchall()[0]
-			raise RuntimeError("File already imported on {0}".format(x[0])) #from None
+			raise RuntimeError("File already imported on {0}".format(x[0]))
 		else:
 			importID = cur.lastrowid
 	return importID
@@ -74,7 +73,8 @@ def process(crude_data, importID):
 	state_ = "staging"
 	import_ = importID
 	url = crude_data.url
-	top = utils.partialDownload(1024)	# TODO: this needs to be incremental if title finding fails
+	# TODO: the following needs to be incremental if title finding fails
+	top = utils.partialDownload(1024)
 	# TODO: this needs to be async
 	title = BeautifulSoup(top).find_all("title")[0].get_text()
 	safeForWork = NotImplemented
@@ -94,12 +94,15 @@ def insertCleanData(clean_data):
 	# http://peter-hoffmann.com/2010/python-sqlite-namedtuple-factory.html
 	with sqliteDB(dbFile) as cur:
 		cur.execute(
-		"INSERT INTO links (url, title, safeForWork, date_created, state_id, import_id) VALUES (?,?,?,?,?,?)"
-		[url, title, safeForWork, date_created, state_id, import_id])
+			"INSERT INTO links "											\
+			"(url, title, safeForWork, date_created, state_id, import_id) "	\
+			"VALUES (?,?,?,?,?,?)",
+			[url, title, safeForWork, date_created, state_id, import_id]
+		)
 		linkID = cur.lastrowid
 	return linkID
 
-def basicIntel(clean_data, old_title, context):
+?def basicIntel(clean_data, old_title, context):
 	info4intel = context
 	if len(old_title) <= len(title):
 		# entire thing is a comment or a true-copy
@@ -114,19 +117,19 @@ def basicIntel(clean_data, old_title, context):
 	return info4intel
 
 def advancedIntel(oldData, newData):
-	# if importID matches, then this should have never happened in the
-	# first place because of the error raised by initNewImport
-	""" derive intel, check reps
-	now now, link can repeat in an entirely different import or in incremental import
-		SO if link && computer && adddate match, ignore
-		if adddate or computer is different increase count in intel
-		if comment is different inform intel
+	""" returns difference based intelligence if any
 	url
 		IS IDENTICAL, thats why we are here
 	title
-		new change worth recording (cuz this is the ORIGINAL title that web-author gave, any comments associated would've been added to link's intel already)
+		no intel is found in the title in this scenario because whatever intel
+		was available was already processed by basicIntel and these titles
+		are extracted from the downloaded webpage
+		**new change worth recording (cuz this is the ORIGINAL title that
+		web-author gave, any comments associated would've been added to
+		link's intel already)
 	safeForWork
 		new change worth recording (with prompt)
+		cuz machine learing api will only get smarter by the day
 	date_created
 		doesn't tell anything alone, look at import_id
 	state_id
@@ -136,6 +139,8 @@ def advancedIntel(oldData, newData):
 	"""
 	final = newData
 	info4intel = list()
+	# if importID matches, then this should have never happened in the
+	# first place because of the error raised by initNewImport
 	assert oldData.import_ != newData.import_
 	if newData.date_created == oldData.date_created:
 		# this is a case of incremental import
@@ -150,18 +155,50 @@ def advancedIntel(oldData, newData):
 		final.title = newData.title
 		info4intel.append(oldData.title)
 	if newData.safeForWork != oldData.safeForWork:
-		# cuz machine learing api will get smarter by the day
 		final.safeForWork = newData.safeForWork
+	# TODO
+	# if newData.state_ != oldData.state_:
+	return final, info4intel
+
+def updateLinkDetails(linkID, clean_data):
+	with sqliteDB(dbFile) as cur:
+		cur.execute(
+			"UPDATE links " 		\
+			"SET " 					\
+			"	url=?, " 			\
+			"	title=?, " 			\
+			"	safeForWork=?, " 	\
+			"	date_created=?, " 	\
+			"	state_id=?, " 		\
+			"	import_id=? " 		\
+			"WHERE " 				\
+			"	linkID=?;"
+			[url, title, safeForWork, date_created, state_id, import_id, \
+			linkID]
+		)
+		linkID = cur.lastrowid
+	return
+
+def insertIntel(linkID, timestamp, infoSet):
+	with sqliteDB(dbFile) as cur:
+		for piece in infoSet:
+			cur.execute(
+				"INSERT INTO intel "\
+				"(FK_linkID, information, ts_on_zAxis) "\
+				"VALUES (?,?,?)", [linkID, str(piece), timestamp]
+			)
 	return
 
 def stage(file_path, raw_data):
 	importID = initNewImport(file_path)
 	for aCrude in raw_data:
-		# ? if url exists in db get it as obj () and old_importID (ts_on_zAxis, computer & location)
+		# ? if url exists in db get it as obj () and old_importID (ts_on_zAxis,
+		# computer & location)
 		# semi = process(aCrude, importID, oldData)
 		# INSERT...
 		context = aCrude.context
 		old_title = aCrude.raw_title
+		timestamp = int(time.time())	# needed for inserting intel data
 		clean_data = process(aCrude, importID)
 		base_intel = basicIntel(clean_data, old_title, context)
 		try:
@@ -169,21 +206,29 @@ def stage(file_path, raw_data):
 		except sqlite3.IntegrityError:
 			# => url already exists, time to increase intel
 			newData = clean_data
-			oldData = utlty_getOldLink(newData.url)
-			diff_based_intel = advancedIntel(oldData, newData)
-			# NOTE: newData.import_ == oldData.import_ will never happen under normal flow of control
-			update_db(newData)
-		finally:
-			insert base_intel & diff_based_intel if it exists INTO `link`
+			linkID, oldData = utlty_getOldLink(newData.url)
+			final, diff_intel = advancedIntel(oldData, newData)
+			# NOTE: newData.import_ == oldData.import_ will never happen under
+			# normal flow of control
+			if final == oldData:
+				pass
+			else:
+				updateLinkDetails(linkID, final)
+				insertIntel(linkID, timestamp, diff_intel)
+		insertIntel(linkID, timestamp, base_intel)
 	return
 
 def utlty_getOldLink(url):
 	with sqliteDB(dbFile) as cur:
 		x = cur.execute(
-		"SELECT url, title, safeForWork, date_created, state_id, import_id FROM links WHERE url=?",
-		[url])
+			"SELECT "							\
+			"linkID, url, title, safeForWork, "	\
+			"date_created, state_id, import_id "\
+			"FROM links WHERE url=?", [url]
+		)
 		x = x.fetchone()
-	ans = Clean(*x)
+	ans = Clean(x[1:])
+	linkID = x[0]
 	assert type(ans.import_id) is int
 	return ans
 
