0d7720ed5b8059fd6701153721d088d098d4b901
s-c-p <ps45@binkmail.com>
2017-09-27 13:44:49 +0530

    tested -- behaviour satisfactoy, styling and posting data left

--- view/ol.js
+++ view/ol.js
@@ -108,7 +108,7 @@ let show_details = function(object) {
 		noteString+= `<li class=a-note>${object.notes[i]}</li>`;
 	};
 	// delete if findin was successful without any errors
-	u("div.expendable-body").remove();
+	u(".expendable-body").remove();
 	// build the final
 	final = details_editor[0].formatUnicorn({
 			link_id: link_id,
@@ -120,6 +120,57 @@ let show_details = function(object) {
 		details_editor[3];
 	// draw
 	u("div.detail").append(final);
+	// activate input region
+	u(".common-input").on("keydown", function (e) {
+		if (e.which == 13) {
+			// avoid form submission on enter key press
+			e.preventDefault();
+			// now on to the logic
+			let val = document.querySelector(".common-input").value;
+			let code = "";
+			let target = "";
+			switch (val[0]) {
+				case "#":
+					code = "<li class=a-tag>" + val;
+					target = ".tags";
+					break;
+				case "@":
+					code = "<li class=a-project>" + val;
+					target = ".projects";
+					break;
+				default:
+					code = "<li class=a-note>" + val;
+					target = ".notes";
+					break;
+			}
+			code += "</li>";
+			u(target).append(code);
+			// clear the input field
+			document.querySelector(".common-input").value = "";
+		}
+	});
+	// activate clear_all button
+	u("#clear_all").on("click", function (e) {
+		u(".expendable-body").remove();
+	});
+	// activate submit
+	u("#save_details").on("click", function (e) {
+		let i = 0;
+		let ans = {};	// as opposed to Object
+		ans.id = document.querySelector(".expendable-body").id;
+		ans.sfw = document.getElementById("sfw").checked;
+		ans.vpn = document.getElementById("vpn").checked;
+		let x = document.querySelectorAll(".a-tag");
+		ans.tags = [];
+		for (i=0; i<x.length; i++) {ans.tags.push(x[i].innerText);}
+		let y = document.querySelectorAll(".a-project");
+		ans.projects = [];
+		for (i=0; i<y.length; i++) {ans.projects.push(y[i].innerText);}
+		let z = document.querySelectorAll(".a-note");
+		ans.notes = [];
+		for (i=0; i<z.length; i++) {ans.notes.push(z[i].innerText);}
+		console.log(ans);
+	});
 };
 
 
