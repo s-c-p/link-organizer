ee9465367421108b08ec031ba1d9c6a9bf976cfb
s-c-p <ps45@binkmail.com>
2017-09-27 11:48:31 +0530

    yo

--- lab/script.js
+++ lab/script.js
@@ -26,3 +26,24 @@ u(".common-input").on("keydown", function (e) {
 		document.querySelector(".common-input").value = "";
 	}
 });
+
+u("#clear_all").on("click", function (e) {
+	alert("clearing all");
+});
+
+u("#save_details").on("click", function (e) {
+	let i = 0;
+	let ans = {};	// as opposed to Object
+	ans.sfw = document.getElementById("sfw").checked;
+	ans.vpn = document.getElementById("vpn").checked;
+	let x = document.querySelectorAll(".a-tag");
+	ans.tags = [];
+	for (i=0; i<x.length; i++) {ans.tags.push(x[i].innerText);}
+	let y = document.querySelectorAll(".a-project");
+	ans.projects = [];
+	for (i=0; i<y.length; i++) {ans.projects.push(y[i].innerText);}
+	let z = document.querySelectorAll(".a-note");
+	ans.notes = [];
+	for (i=0; i<z.length; i++) {ans.notes.push(z[i].innerText);}
+	console.log(ans);
+});
