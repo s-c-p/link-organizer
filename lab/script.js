6874f91b1ad8ad0ce51c6fdc4416094683f1cf15
s-c-p <ps45@binkmail.com>
2017-09-27 00:12:37 +0000

    put the data inside FORM tag and modified JS accordingly

--- lab/script.js
+++ lab/script.js
@@ -1,5 +1,28 @@
-u("common-input").on("keydown", function (e) {
+u(".common-input").on("keydown", function (e) {
 	if (e.which == 13) {
-		alert("13")
+		// avoid form submission on enter key press
+		e.preventDefault();
+		// now on to the logic
+		let val = document.querySelector(".common-input").value;
+		let code = "";
+		let target = "";
+		switch (val[0]) {
+			case "#":
+				code = "<li class=a-tag>" + val;
+				target = ".tags";
+				break;
+			case "@":
+				code = "<li class=a-project>" + val;
+				target = ".projects";
+				break;
+			default:
+				code = "<li class=a-note>" + val;
+				target = ".notes";
+				break;
+		}
+		code += "</li>";
+		u(target).append(code);
+		// clear the input field
+		document.querySelector(".common-input").value = "";
 	}
 });
