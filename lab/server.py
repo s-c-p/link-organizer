6be08145789ca03c34856e37cf4cb91573fa5ef9
s-c-p <ps45@binkmail.com>
2017-09-28 16:00:05 +0000

    yo

--- lab/server.py
+++ lab/server.py
@@ -10,9 +10,37 @@ DATA = \
 , {"id": 6, "url": "http://example.com", "swf": True, "vpn": False, "title": "Zaphod Beelebrox was an idiot", "tags": ["a", "b", "c"], "projects": ["xx", "yy", "zz"], "notes": ['id: 6', 'url: "http://example.com"', 'swf: true, vpn: false']}
 , {"id": 7, "url": "http://example.com", "swf": True, "vpn": False, "title": "Benji mouse, I wanna be like him", "tags": ["a", "b", "c"], "projects": ["xx", "yy", "zz"], "notes": ['id: 7', 'url: "http://example.com"', 'swf: true, vpn: false']}];
 
+# DATA = {"menu": {
+#     "header": "SVG Viewer",
+#     "items": [
+#         {"id": "Open"},
+#         {"id": "OpenNew", "label": "Open New"},
+#         "null",
+#         {"id": "ZoomIn", "label": "Zoom In"},
+#         {"id": "ZoomOut", "label": "Zoom Out"},
+#         {"id": "OriginalView", "label": "Original View"},
+#         "null",
+#         {"id": "Quality"},
+#         {"id": "Pause"},
+#         {"id": "Mute"},
+#         "null",
+#         {"id": "Find", "label": "Find..."},
+#         {"id": "FindAgain", "label": "Find Again"},
+#         {"id": "Copy"},
+#         {"id": "CopyAgain", "label": "Copy Again"},
+#         {"id": "CopySVG", "label": "Copy SVG"},
+#         {"id": "ViewSVG", "label": "View SVG"},
+#         {"id": "ViewSource", "label": "View Source"},
+#         {"id": "SaveAs", "label": "Save As"},
+#         "null",
+#         {"id": "Help"},
+#         {"id": "About", "label": "About Adobe CVG Viewer..."}
+#     ]
+# }}
 
 @bottle.route("/populate")
 def populate():
+    bottle.response.content_type = 'application/json'
     return json.dumps(DATA)
 
 
