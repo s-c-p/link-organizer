de4e4429913c0c4dcae298e78c9fa2aa51058582
s-c-p <ps45@binkmail.com>
2017-09-29 11:23:49 +0000

    yo; /populate needs sessionID and pageNum

--- controller.py
+++ controller.py
@@ -51,7 +51,6 @@ def organize_links():
 			session = import_sessions[0]
 			bottle.redirect(f"view/organize_links?session={session}")
 	# obviously else
-	
 	return
 
 # import wizard---------------------------------------------------------------
